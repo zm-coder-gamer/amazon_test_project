bb1c9fd4eeffedb34f2631b74b0287e1acd29fb6
Fawad Malik <fawad.malik@hotmail.com>
2023-10-26 00:19:59 -0700

    Try 1

--- spec/amazon_spec.rb
+++ spec/amazon_spec.rb
@@ -9,8 +9,7 @@ require_relative 'pages/shopping_cart_page'
 RSpec.describe 'Amazon.ca Web Tests' do
   before(:each) do
     options = Selenium::WebDriver::Chrome::Options.new
-    options.add_argument('--headless')
-    options.add_argument('--disable-gpu')
+    options.add_argument('--headless') # Enable headless mode
     @driver = Selenium::WebDriver.for :chrome, options: options
     @home_page = AmazonHomePage.new(@driver)
     @search_results_page = SearchResultsPage.new(@driver)
@@ -32,12 +31,4 @@ RSpec.describe 'Amazon.ca Web Tests' do
     @search_results.each { |title| puts title }
   end
 
-  it 'should search for a product and add it to the shopping cart' do
-    @home_page.navigate_to_amazon
-    @home_page.search_for('Hyperfibre squash')
-    @search_results_page.click_first_search_result_item
-    @shopping_cart_page.add_to_cart
-
-    expect(@shopping_cart_page.verify_cart_contains_product('Hyperfibre squash')).to be true
-  end
 end
